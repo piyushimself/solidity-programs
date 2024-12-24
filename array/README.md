113649ab0fd54bd1b54f5d47a72b7341269a3d51
Piyush Jian <piyush24jain@gmail.com>
2024-12-25 00:05:08 +0530

    array operations

---
+++ array/README.md
@@ -0,0 +1,11 @@
+**Array Operations**: Append, update, and delete array elements.
+
+**Concept**: Array
+
+Extra: 
+
+for push, give input
+
+for pop no need.
+
+Delete can be multiple type in arrary, one in which we don't reduce index, place 0 what deletes.
---
+++ array/array.sol
@@ -0,0 +1,38 @@
+//SPDX-License-Identifier: MIT License
+pragma solidity 0.8.28;
+
+contract Array {
+    uint256[] public num;
+
+    function append(uint256 a) public {
+        num.push(a);
+    }
+
+    function popnum() public {
+        num.pop();
+    }
+
+    function deletenum(uint256 index) public {
+        require(index < num.length, "Index out of bounds");
+        num[index] = 0;
+    }
+
+    function update(uint256 index, uint256 newnum) public {
+        require(index <= num.length, "Index out of bounds");
+        num[index] = newnum;
+    }
+
+    function deletewithoutgap(uint256 index) public {
+        require(index < num.length, "Index out of bounds");
+        num[index] = num[num.length - 1];
+        num.pop();
+    }
+
+    function arrlength() public view returns (uint256) {
+        return num.length;
+    }
+
+    function printArray() public view returns (uint256[] memory) {
+        return num;
+    }
+}
